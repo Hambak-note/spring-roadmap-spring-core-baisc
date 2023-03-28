971cb904064a0b3554b715af8ed31f4447fd3bc3
Hambak-note <dudghks5722@gmail.com>
2023-03-28 22:01:31 +0900

    feat: 주문과 할인 서비스 테스트 코드 작성

---
+++ code/src/test/java/spring/code/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package spring.code.order;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import spring.code.member.Grade;
+import spring.code.member.Member;
+import spring.code.member.MemberService;
+import spring.code.member.MemberServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
