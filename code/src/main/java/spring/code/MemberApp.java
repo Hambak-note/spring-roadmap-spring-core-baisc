b0a9bc6953d377c2c9e5b710a0c893d1d6e4f5dd
Hambak-note <dudghks5722@gmail.com>
2023-03-28 21:40:19 +0900

    feat: 회원 도메인 실행 클래스 추가

---
+++ code/src/main/java/spring/code/MemberApp.java
@@ -0,0 +1,21 @@
+package spring.code;
+
+import spring.code.member.Grade;
+import spring.code.member.Member;
+import spring.code.member.MemberService;
+import spring.code.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
